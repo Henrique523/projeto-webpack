99daf64b7d1b8b795652c54f246cef886f5bd6c8
Henrique <suporte@pcmsolucoes.com.br>
2020-06-08 11:11:39 -0300

    Final parte 5.

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,6 +1,7 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/js/modal';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const controller = new NegociacaoController();
